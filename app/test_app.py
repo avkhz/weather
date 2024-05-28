f6e5fb42057ce64f1c5212c7803a9c8ac41a7996
avihu.hababo <avihulink@gmail.com>
2024-05-28 12:57:15 +0300

    test

---
+++ app/test_app.py
@@ -0,0 +1,9 @@
+from app import app
+
+def test_main():
+                # Creates a test client for this application.
+    response = app.test_client().get('/') 
+
+                # assert the stutus code of the page('/') is 200
+    assert response.status_code == 200 
+
