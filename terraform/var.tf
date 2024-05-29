ea22e7ef1fe080a99d30ce5462e66fbe94e93fcc
avihu.hababo <avihulink@gmail.com>
2024-05-29 13:18:05 +0300

    d

---
+++ terraform/var.tf
@@ -0,0 +1,31 @@
+variable "key_name" {
+  default = "dovekey"
+}
+
+variable "file_name" {
+  default = "dovekey.pem"
+}
+
+variable "pub_key_name" {
+  default = "dovekey.pub"
+}
+
+variable "region" {
+  default = "eu-north-1"
+}
+
+variable "cluster_name" {
+  default = "avihu_cluster"
+}
+
+variable "ami" {
+  default = "ami-0705384c0b33c194c"
+}
+
+variable "master_node_ec2_type" {
+  default = "t3.micro"
+}
+
+variable "worker_node_ec2_type" {
+  default = "t3.small"
+}
